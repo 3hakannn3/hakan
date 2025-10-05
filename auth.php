0a8a8324a6e2e1b843f9c338af406d25481492ad
3hakannn3 <3hakannn3@gmail.com>
2025-10-06 00:24:26 +0300

    Delete auth.php

--- auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once __DIR__.'/../src/DB.php';
-$method = $_SERVER['REQUEST_METHOD'];
-header('Content-Type: application/json; charset=utf-8');
-if ($method === 'DELETE') { session_destroy(); echo json_encode(['ok'=>true]); exit; }
-echo json_encode(['ok'=>true]);
